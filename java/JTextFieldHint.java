9c05a456cf83bea3e8c3303bc27cee28936dd8d7
johneliades <johneliades@hotmail.com>
2020-02-19 18:02:52 +0200

    removing unecessary things

--- java/JTextFieldHint.java
+++ java/JTextFieldHint.java
@@ -3,7 +3,7 @@ import javax.swing.border.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class JTextFieldHint extends JTextField implements FocusListener {
+public class JTextFieldHint extends JTextField {
 	private JTextField jtf;
 	private Icon icon;
 	private Insets dummyInsets;
@@ -15,8 +15,6 @@ public class JTextFieldHint extends JTextField implements FocusListener {
 		Border border = UIManager.getBorder("TextField.border");
 		JTextField dummy = new JTextField();
 		this.dummyInsets = border.getBorderInsets(dummy);
-
-		addFocusListener(this);
 	}
 
 	public void setIcon(Icon newIcon){
@@ -25,46 +23,35 @@ public class JTextFieldHint extends JTextField implements FocusListener {
 
 	@Override
 	protected void paintComponent(Graphics g) {
-			super.paintComponent(g);
+		super.paintComponent(g);
 
-			int textX = 2;
+		int textX = 2;
 
-			if(this.icon!=null){
-				int iconWidth = icon.getIconWidth();
-				int iconHeight = icon.getIconHeight();
-				int x = dummyInsets.left + 5;
-				textX = x+iconWidth+2;
-				int y = (this.getHeight() - iconHeight)/2;
-				icon.paintIcon(this, g, x, y);
-			}
+		if(this.icon!=null){
+			int iconWidth = icon.getIconWidth();
+			int iconHeight = icon.getIconHeight();
+			int x = dummyInsets.left + 5;
+			textX = x+iconWidth+2;
+			int y = (this.getHeight() - iconHeight)/2;
+			icon.paintIcon(this, g, x, y);
+		}
 
-			setMargin(new Insets(2, textX, 2, 2));
+		setMargin(new Insets(2, textX, 2, 2));
 
-			if(this.getText().equals("")) {
-				int width = this.getWidth();
-				int height = this.getHeight();
-				Font prev = g.getFont();
-				Font italic = prev.deriveFont(Font.ITALIC);
-				Color prevColor = g.getColor();
-				g.setFont(italic);
-				g.setColor(UIManager.getColor("textInactiveText"));
-				int h = g.getFontMetrics().getHeight();
-				int textBottom = (height - h) / 2 + h - 4;
-				int x = this.getInsets().left;
-				Graphics2D g2d = (Graphics2D) g;
-				g.setFont(prev);
-				g.setColor(prevColor);
-			}
+		if(this.getText().equals("")) {
+			int width = this.getWidth();
+			int height = this.getHeight();
+			Font prev = g.getFont();
+			Font italic = prev.deriveFont(Font.ITALIC);
+			Color prevColor = g.getColor();
+			g.setFont(italic);
+			g.setColor(UIManager.getColor("textInactiveText"));
+			int h = g.getFontMetrics().getHeight();
+			int textBottom = (height - h) / 2 + h - 4;
+			int x = this.getInsets().left;
+			Graphics2D g2d = (Graphics2D) g;
+			g.setFont(prev);
+			g.setColor(prevColor);
+		}
 	}
-
-	@Override
-	public void focusGained(FocusEvent arg0) {
-		this.repaint();
-	}
-
-	@Override
-	public void focusLost(FocusEvent arg0) {
-		this.repaint();
-	}
-
 }
