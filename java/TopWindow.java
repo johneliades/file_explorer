7d26b831ea4bca48f758657c389dd11e01538e9a
johneliades <johneliades@hotmail.com>
2021-03-02 20:29:12 +0200

    bug fix

--- java/TopWindow.java
+++ java/TopWindow.java
@@ -475,10 +475,16 @@ public class TopWindow extends JPanel {
 			JButton button;
 
 			String name = file.getName();
-			if(name.trim().length() == 0 || name.equals("/")) {
-				button = new JButton(Utility.getImageFast(
-					FileExplorer.getIconPath() + "other/pc.png", 
-					navHeight-4, navHeight-7, true));	
+			if(name.trim().length() == 0) {
+				if(file.getPath().equals("/") || name.equals(windowsTopName)) {
+					button = new JButton(Utility.getImageFast(
+						FileExplorer.getIconPath() + "other/pc.png", 
+						navHeight-4, navHeight-7, true));	
+				}
+				else {
+					name = "Local Disk (" + file.getPath().replace("\\", "") + ")";
+					button = new JButton(name);		
+				}
 			}
 			else {
 				button = new JButton(name);	
