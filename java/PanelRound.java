2c57e0c6f3dab4791e2e161232e79875ba70a21c
johneliades <johneliades@hotmail.com>
2022-11-24 00:36:01 +0200

    changed indentation

--- java/PanelRound.java
+++ java/PanelRound.java
@@ -10,128 +10,128 @@ import javax.swing.JPanel;
 import java.awt.*;
 
 public class PanelRound extends JPanel {
-    private int roundTopLeft = 0;
-    private int roundTopRight = 0;
-    private int roundBottomLeft = 0;
-    private int roundBottomRight = 0;
-    private Color cornerColor = null;
+	private int roundTopLeft = 0;
+	private int roundTopRight = 0;
+	private int roundBottomLeft = 0;
+	private int roundBottomRight = 0;
+	private Color cornerColor = null;
 
-    public void setCornerColor(Color cornerColor) {
-        this.cornerColor = cornerColor;
-        repaint();
-    }
+	public void setCornerColor(Color cornerColor) {
+		this.cornerColor = cornerColor;
+		repaint();
+	}
 
-    public int getRoundTopLeft() {
-        return roundTopLeft;
-    }
+	public int getRoundTopLeft() {
+		return roundTopLeft;
+	}
 
-    public void setRoundTopLeft(int roundTopLeft) {
-        this.roundTopLeft = roundTopLeft;
-        repaint();
-    }
+	public void setRoundTopLeft(int roundTopLeft) {
+		this.roundTopLeft = roundTopLeft;
+		repaint();
+	}
 
-    public int getRoundTopRight() {
-        return roundTopRight;
-    }
+	public int getRoundTopRight() {
+		return roundTopRight;
+	}
 
-    public void setRoundTopRight(int roundTopRight) {
-        this.roundTopRight = roundTopRight;
-        repaint();
-    }
+	public void setRoundTopRight(int roundTopRight) {
+		this.roundTopRight = roundTopRight;
+		repaint();
+	}
 
-    public int getRoundBottomLeft() {
-        return roundBottomLeft;
-    }
+	public int getRoundBottomLeft() {
+		return roundBottomLeft;
+	}
 
-    public void setRoundBottomLeft(int roundBottomLeft) {
-        this.roundBottomLeft = roundBottomLeft;
-        repaint();
-    }
+	public void setRoundBottomLeft(int roundBottomLeft) {
+		this.roundBottomLeft = roundBottomLeft;
+		repaint();
+	}
 
-    public int getRoundBottomRight() {
-        return roundBottomRight;
-    }
+	public int getRoundBottomRight() {
+		return roundBottomRight;
+	}
 
-    public void setRoundBottomRight(int roundBottomRight) {
-        this.roundBottomRight = roundBottomRight;
-        repaint();
-    }
+	public void setRoundBottomRight(int roundBottomRight) {
+		this.roundBottomRight = roundBottomRight;
+		repaint();
+	}
 
-    public PanelRound(LayoutManager layout, int a, int b, int c, int d, Color color) {
-        super(layout);
-        setOpaque(false);
-        roundTopLeft = a;
-        roundTopRight = b;
-        roundBottomLeft = c;
-        roundBottomRight = d; 
-        cornerColor = color;
-    }
+	public PanelRound(LayoutManager layout, int a, int b, int c, int d, Color color) {
+		super(layout);
+		setOpaque(false);
+		roundTopLeft = a;
+		roundTopRight = b;
+		roundBottomLeft = c;
+		roundBottomRight = d; 
+		cornerColor = color;
+	}
 
-    @Override
-    protected void paintComponent(Graphics grphcs) {
-        Graphics2D g2 = (Graphics2D) grphcs.create();
-        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+	@Override
+	protected void paintComponent(Graphics grphcs) {
+		Graphics2D g2 = (Graphics2D) grphcs.create();
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-        g2.setColor(cornerColor);
-        g2.fillRect(0, 0, getWidth(), getHeight());
+		g2.setColor(cornerColor);
+		g2.fillRect(0, 0, getWidth(), getHeight());
 
-        g2.setColor(getBackground());
-        Area area = new Area(createRoundTopLeft());
-        if (roundTopRight > 0) {
-            area.intersect(new Area(createRoundTopRight()));
-        }
-        if (roundBottomLeft > 0) {
-            area.intersect(new Area(createRoundBottomLeft()));
-        }
-        if (roundBottomRight > 0) {
-            area.intersect(new Area(createRoundBottomRight()));
-        }
-        g2.fill(area);
-        g2.dispose();
-        super.paintComponent(grphcs);
-    }
+		g2.setColor(getBackground());
+		Area area = new Area(createRoundTopLeft());
+		if (roundTopRight > 0) {
+			area.intersect(new Area(createRoundTopRight()));
+		}
+		if (roundBottomLeft > 0) {
+			area.intersect(new Area(createRoundBottomLeft()));
+		}
+		if (roundBottomRight > 0) {
+			area.intersect(new Area(createRoundBottomRight()));
+		}
+		g2.fill(area);
+		g2.dispose();
+		super.paintComponent(grphcs);
+	}
 
-    private Shape createRoundTopLeft() {
-        int width = getWidth();
-        int height = getHeight();
-        int roundX = Math.min(width, roundTopLeft);
-        int roundY = Math.min(height, roundTopLeft);
-        Area area = new Area(new RoundRectangle2D.Double(0, 0, width, height, roundX, roundY));
-        area.add(new Area(new Rectangle2D.Double(roundX / 2, 0, width - roundX / 2, height)));
-        area.add(new Area(new Rectangle2D.Double(0, roundY / 2, width, height - roundY / 2)));
-        return area;
-    }
+	private Shape createRoundTopLeft() {
+		int width = getWidth();
+		int height = getHeight();
+		int roundX = Math.min(width, roundTopLeft);
+		int roundY = Math.min(height, roundTopLeft);
+		Area area = new Area(new RoundRectangle2D.Double(0, 0, width, height, roundX, roundY));
+		area.add(new Area(new Rectangle2D.Double(roundX / 2, 0, width - roundX / 2, height)));
+		area.add(new Area(new Rectangle2D.Double(0, roundY / 2, width, height - roundY / 2)));
+		return area;
+	}
 
-    private Shape createRoundTopRight() {
-        int width = getWidth();
-        int height = getHeight();
-        int roundX = Math.min(width, roundTopRight);
-        int roundY = Math.min(height, roundTopRight);
-        Area area = new Area(new RoundRectangle2D.Double(0, 0, width, height, roundX, roundY));
-        area.add(new Area(new Rectangle2D.Double(0, 0, width - roundX / 2, height)));
-        area.add(new Area(new Rectangle2D.Double(0, roundY / 2, width, height - roundY / 2)));
-        return area;
-    }
+	private Shape createRoundTopRight() {
+		int width = getWidth();
+		int height = getHeight();
+		int roundX = Math.min(width, roundTopRight);
+		int roundY = Math.min(height, roundTopRight);
+		Area area = new Area(new RoundRectangle2D.Double(0, 0, width, height, roundX, roundY));
+		area.add(new Area(new Rectangle2D.Double(0, 0, width - roundX / 2, height)));
+		area.add(new Area(new Rectangle2D.Double(0, roundY / 2, width, height - roundY / 2)));
+		return area;
+	}
 
-    private Shape createRoundBottomLeft() {
-        int width = getWidth();
-        int height = getHeight();
-        int roundX = Math.min(width, roundBottomLeft);
-        int roundY = Math.min(height, roundBottomLeft);
-        Area area = new Area(new RoundRectangle2D.Double(0, 0, width, height, roundX, roundY));
-        area.add(new Area(new Rectangle2D.Double(roundX / 2, 0, width - roundX / 2, height)));
-        area.add(new Area(new Rectangle2D.Double(0, 0, width, height - roundY / 2)));
-        return area;
-    }
+	private Shape createRoundBottomLeft() {
+		int width = getWidth();
+		int height = getHeight();
+		int roundX = Math.min(width, roundBottomLeft);
+		int roundY = Math.min(height, roundBottomLeft);
+		Area area = new Area(new RoundRectangle2D.Double(0, 0, width, height, roundX, roundY));
+		area.add(new Area(new Rectangle2D.Double(roundX / 2, 0, width - roundX / 2, height)));
+		area.add(new Area(new Rectangle2D.Double(0, 0, width, height - roundY / 2)));
+		return area;
+	}
 
-    private Shape createRoundBottomRight() {
-        int width = getWidth();
-        int height = getHeight();
-        int roundX = Math.min(width, roundBottomRight);
-        int roundY = Math.min(height, roundBottomRight);
-        Area area = new Area(new RoundRectangle2D.Double(0, 0, width, height, roundX, roundY));
-        area.add(new Area(new Rectangle2D.Double(0, 0, width - roundX / 2, height)));
-        area.add(new Area(new Rectangle2D.Double(0, 0, width, height - roundY / 2)));
-        return area;
-    }
+	private Shape createRoundBottomRight() {
+		int width = getWidth();
+		int height = getHeight();
+		int roundX = Math.min(width, roundBottomRight);
+		int roundY = Math.min(height, roundBottomRight);
+		Area area = new Area(new RoundRectangle2D.Double(0, 0, width, height, roundX, roundY));
+		area.add(new Area(new Rectangle2D.Double(0, 0, width - roundX / 2, height)));
+		area.add(new Area(new Rectangle2D.Double(0, 0, width, height - roundY / 2)));
+		return area;
+	}
 }
